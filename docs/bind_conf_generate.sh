de4ba1459aed1e7d6643379754990de8c5ac87d6
Heera Koranga <heerakoranga@gmail.com>
2013-10-18 11:16:23 +0530

    script modified.

--- docs/bind_conf_generate.sh
+++ docs/bind_conf_generate.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+#####################################################
+### Generate BIND Configuration Using This Script ###
+#####################################################
+
+
 #http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 #http://dl.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
 
